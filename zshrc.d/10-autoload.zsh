c7bf625febd910df69b1d589b0dc6343e8f12fd2
onokatio <onokatio@gmail.com>
2017-03-30 23:37:04 +0900

    zshrc.d

---
+++ zshrc.d/10-autoload.zsh
@@ -0,0 +1,6 @@
+autoload -Uz compinit && compinit -C
+autoload -Uz colors && colors
+autoload -Uz add-zsh-hook
+autoload -Uz select-word-style && select-word-style default
+autoload -Uz vcs_info
+autoload -Uz zed
