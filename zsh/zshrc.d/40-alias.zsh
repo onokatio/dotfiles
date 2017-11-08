0a80844f23619ea6e62d20fa0988a4aeec4f0332
onokatio <onokatio@gmail.com>
2017-11-08 14:52:38 +0900

    zsh

--- zsh/zshrc.d/40-alias.zsh
+++ zsh/zshrc.d/40-alias.zsh
@@ -2,33 +2,44 @@ alias mkdir='mkdir -p '
 alias d='popd '
 alias bat="upower -i /org/freedesktop/UPower/devices/battery_BAT0|grep 'percentage'|sed -e 's/ //g'|sed -e 's/percentage://'|sed -e 's/%//' "
 alias gcc-win="/usr/bin/i686-w64-mingw32-gcc "
-alias nano="vim "
+#alias nano="vim "
 #alias grep="grep --color -n -I "
 alias ls='ls -hqp --color=auto '
 alias l='ls -hqp --color=auto '
-alias wget='axel -n 10 -v -a '
 alias make='make -j4 '
 alias sudo='sudo -E '
-alias apt='apt-fast '
-alias apt-get='apt-fast '
 alias crontab='echo "dont use crontab !!" '
-alias chromef='ps ax|/bin/grep [c]hrome|cut -c-6|xargs sudo renice -19 '
+alias chromekill='ps ax|/bin/grep [c]hrome|cut -c-6|xargs sudo renice -19 '
 alias pingg="ping 8.8.8.8 "
 alias curlg="curl https://google.com "
 alias alpine="docker run -it --rm alpine:edge /bin/sh "
-alias df='dfc '
-alias top='htop '
-alias diff='colordiff '
-alias info='pinfo '
 #alias cat='vimcat '
 alias trace-mtr='mtr '
 #alias grep='ag '
-alias rm="gomi --"
 #alias rm='rm -vrI --one-file-system -- '
-alias s='w3m https://google.com/search?q='
+#alias s='w3m https://google.com/search?q='
 alias poweroff=': '
-alias vi='nvim -u ~/.vimrc '
-alias vim='nvim -u ~/.vimrc '
+
+#alias df='dfc '
+#alias top='htop '
+#alias diff='colordiff '
+#alias info='pinfo '
+#alias apt='apt-fast '
+#alias apt-get='apt-fast '
+#alias wget='axel -n 10 -v -a '
+#alias vi='nvim -u ~/.vimrc '
+#alias vim='nvim -u ~/.vimrc '
+#alias rm="gomi --"
+
+cmd=(df  top  diff      info  apt      apt-get  wget               vi                 vim                rm)
+rpc=(dfc htop colordiff pinfo apt-fast apt-fast 'axel -n 10 -v -a' 'nvim -u ~/.vimrc' 'nvim -u ~/.vimrc' gomi)
+
+for i in {1..$#cmd};do
+	if which $(echo $rpc[$i]|awk '{print $1}') >/dev/null; then
+		alias "$cmd[$i]"="$rpc[$i]";
+	fi
+done
+
 
 function git(){hub "$@"}
 function chrome(){
