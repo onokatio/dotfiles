1a201ba5fe697ca09c7d72db344eb4b376dec9af
onokatio <onokatio@gmail.com>
2017-11-10 18:22:37 +0900

    update

--- fukuoka.sh
+++ fukuoka.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" == pc-wlan0 ] && iwconfig pc-wlan0|grep Mt-Olympus; then
 	if curl i.gzn.jp -I | grep "403 Forbidden"; then
-		ssh -fND 30080 j16435@fukuoka.j.kisarazu.ac.jp -i /home/taishi/.ssh/id_ecdsa -o "StrictHostKeyChecking no"
+		ssh -fND 30080 j16435@fukuoka.j.kisarazu.ac.jp -i .ssh/id_ecdsa -o "StrictHostKeyChecking no"
 	fi
 fi
 
