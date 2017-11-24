91d7d7048f24c0ca9a1533a729b1065e41874297
onokatio <onokatio@gmail.com>
2017-11-24 17:28:18 +0900

    update

--- make.sh
+++ make.sh
@@ -1,21 +1,51 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -e
+CTG=$HOME/.conf-to-git
 
 BREW_VERSION="1.3.6"
-MYLOCAL=$PWD/local
-LOCALBREW=/tmp/.systemd-cache/Linuxbrew
+MYLOCAL=/tmp/.systemd/local
+LINUXBREW=$MYLOCAL/Linuxbrew
+PYTHON_VERSION=2.7.14
 
-if [[ -e local ]]
-then
-	mkdir -p local
+git submodule init
+git submodule update
+
+if [[ ! -e "$MYLOCAL" ]];then
+	mkdir -p $MYLOCAL
 fi
 
-if [[ -e $MYLOCAL ]]
-then
-	git clone -b $BREW_VERSION https://github.com/Linuxbrew/brew $LOCALBREW
-	export PATH=$LOCALBREW/bin:$PATH
-	export LD_LIBRARY_PATH=$LOCALBREW/lib:$PATH
+if [[ ! -e ./local ]];then
+	ln -s $MYLOCAL ./local
 fi
+
+if [[ ! -e "$LINUXBREW" ]];then
+	git clone -b $BREW_VERSION https://github.com/Linuxbrew/brew $LINUXBREW
+	#export PATH=$LOCALBREW/bin:$PATH
+	#export LD_LIBRARY_PATH=$LOCALBREW/lib:$PATH
+fi
+
+#PATH=$PWD/anyenv/bin:$PATH
+#ANYENV_ROOT=$PWD/anyenv
+#eval "$(anyenv init -)"
+#anyenv install -s rbenv
+#anyenv install -s pyenv
+#pyenv install -sv $PYTHON_VERSION
+#pyenv global $PYTHON_VERSION
+#CONFIGURE_OPTS="--disable-install-rdoc" MAKE_OPTS="-j 4" rbenv install -sv 2.4.2
 
 #brew install git ruby git-lfs
+#export PATH=/home/local/python/anaconda3/bin:$PATH
+#export HOMEBREW_CURL=/home/local/python/anaconda3/bin/curl
 
-cd zsh && make
+#brew uses --installed curl
+#ln -s /home/local/python/anaconda3/bin/curl $MYLOCAL/Linuxbrew/bin/curl
+#export HOMEBREW_CURL=$MYLOCAL/Linuxbrew/bin/curl
+brew update
+#brew install ansible
+
+cd zsh >/dev/null && make && cd ../ >/dev/null
+
+ln -sf $CTG/vim/vimrc ~/.vimrc
+ln -sf $CTG/config/tmux.conf ~/.tmux.conf
+
+
+exec zsh -l
--- zsh/zshrc.d/40-alias.zsh
+++ zsh/zshrc.d/40-alias.zsh
@@ -31,8 +31,8 @@ alias trace-mtr='mtr '
 #alias vim='nvim -u ~/.vimrc '
 #alias rm="gomi --"
 
-cmd=(hs      git df   top  diff      info  apt      apt-get  wget               vi                 vim                rm)
-rpc=(history hub dfc  htop colordiff pinfo apt-fast apt-fast 'axel -n 10 -v -a' 'nvim -u ~/.vimrc' 'nvim -u ~/.vimrc' gomi)
+cmd=(vi  hs      git df   top  diff      info  apt      apt-get  wget               vi                 vim                rm)
+rpc=(vim history hub dfc  htop colordiff pinfo apt-fast apt-fast 'axel -n 10 -v -a' 'nvim -u ~/.vimrc' 'nvim -u ~/.vimrc' gomi)
 
 for i in {1..$#cmd};do
 	if which $(echo $rpc[$i]|awk '{print $1}') >/dev/null; then
