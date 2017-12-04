bc0af5dd19ec7479d5bef5b2844bfea622a94d15
onokatio <onokatio@gmail.com>
2017-12-04 13:56:38 +0900

    update

--- zsh/zshrc.d/40-alias.zsh
+++ zsh/zshrc.d/40-alias.zsh
@@ -45,6 +45,15 @@ function chrome(){
 	chrome_b=$(ps ax|env grep '/opt/google/chrome'|head -n1|awk '{print $5}'|cut -d '/' -f 4)
 	eval "google-$chrome_b $@"
 }
+#function brew(){
+#	if [[ "$1" == "install" ]];then
+#		shift
+#		echo '$BREW' "$@"
+#		brew install $@
+#	else
+#		brew $@
+#	fi
+#}
 function targz(){tar xvf $@ --use-compress-prog=pigz }
 function tarxz(){tar xvf $@ --use-compress-prog=pixz }
 function tarbzip2(){tar xvf $@ --use-compress-prog=pbzip2 }
