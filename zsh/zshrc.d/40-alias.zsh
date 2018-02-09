73bb914d52046e41bba5bf556b97285db2d83ba4
onokatio <onokatio@gmail.com>
2018-02-09 11:56:20 +0900

    change alias system

--- zsh/zshrc.d/40-alias.zsh
+++ zsh/zshrc.d/40-alias.zsh
@@ -1,53 +1,52 @@
-alias mkdir='mkdir -p '
-alias d='popd '
-alias bat="upower -i /org/freedesktop/UPower/devices/battery_BAT0|grep 'percentage'|sed -e 's/ //g'|sed -e 's/percentage://'|sed -e 's/%//' "
-alias gcc-win="/usr/bin/i686-w64-mingw32-gcc "
-#alias nano="vim "
-#alias grep="grep --color -n -I "
-alias ls='ls -hqp --color=auto '
-alias l='ls -hqp --color=auto '
-alias make='make -j4 '
-alias sudo='sudo -E '
-alias crontab='echo "dont use crontab !!" '
-alias chromekill='ps ax|/bin/grep [c]hrome|cut -c-6|xargs sudo renice -19 '
-alias pingg="ping 8.8.8.8 "
-alias curlg="curl https://google.com "
-alias alpine="docker run -it --rm alpine:edge /bin/sh "
-#alias cat='vimcat '
-alias trace-mtr='mtr '
-alias brew='brew -v '
-alias open='xdg-open '
-#alias grep='ag '
-#alias rm='rm -vrI --one-file-system -- '
-#alias s='w3m https://google.com/search?q='
-alias poweroff=': '
-sdo() sudo zsh -c "$functions[$1]" "$@"
-#alias sudo="sdo"
+## sdo() sudo zsh -c "$functions[$1]" "$@"
+#fch sudo="sdo"
 
-function alias-check(){
-	if which ${${=2}[1]} > /dev/null 2>$1;then
-		alias "$1"="$2"
+function ach(){
+	if which ${${=2}[1]} > /dev/null 2>&1;then
+		alias "$1"="$2 "
 	fi
 }
-function func-check(){
-	if which ${${=2}[1]} > /dev/null 2>$1;then
-		eval "function $1(){ $2 \$@ }"
+function fch(){
+	local cmd=$(echo $2|cut -d ' ' -f 1)
+	if which $cmd > /dev/null 2>&1;then
+		eval "function $1(){ command $2 \$@ }"
 	fi
 }
 
-alias-check vi "nvim -u ~/.vimrc "
-alias-check vim "nvim -u ~/.vimrc "
+alias sudo='sudo '
+ach vi "nvim -u $HOME/.vimrc "
+ach vim "nvim -u $HOME/.vimrc "
 
-func-check hs			history
-func-check git		hub
-func-check df			dfc
-func-check top		htop
-func-check diff		colordiff
-func-check info		pinfo
-func-check wget		"axel -n 10 -v -a"
-func-check ks			ls
-func-check apt			apt-fast
-func-check apt-get	apt-fast
+fch hs history
+fch git hub
+fch df dfc
+fch top htop
+fch diff colordiff
+fch info pinfo
+fch wget "axel -n 10 -v -a"
+fch ks ls
+fch apt apt-fast
+fch apt-get apt-fast
+fch mkdir 'mkdir -p '
+fch d 'popd '
+fch bat "upower -i /org/freedesktop/UPower/devices/battery_BAT0|grep 'percentage'|sed -e 's/ //g'|sed -e 's/percentage://'|sed -e 's/%//' "
+fch grep "grep --color -n -I "
+fch ls 'ls -hqp --color=auto '
+fch l 'ls -hqp --color=auto '
+fch make 'make -j4 '
+fch sudo 'sudo -E '
+fch crontab 'echo "dont use crontab !!" '
+fch chromekill 'ps ax|/bin/grep [c]hrome|cut -c-6|xargs sudo renice -19 '
+fch pingg "ping 8.8.8.8 "
+fch curlg "curl https://google.com "
+fch alpine "docker run -it --rm alpine:edge /bin/sh "
+fch cat 'vimcat '
+fch trace-mtr 'mtr '
+fch brew 'brew -v '
+fch open 'xdg-open '
+fch rm 'rm -v --one-file-system '
+fch poweroff 'echo dontuse '
+fch reboot 'echo dontuse '
 
 function chrome(){
 	chrome_b=$(ps ax|env grep '/opt/google/chrome'|head -n1|awk '{print $5}'|cut -d '/' -f 4)
