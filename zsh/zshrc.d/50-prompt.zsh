981c43809bc9cdaf77e21647d10656fad6e968f2
onokatio <onokatio@gmail.com>
2018-02-20 12:17:35 +0900

    update

--- zsh/zshrc.d/50-prompt.zsh
+++ zsh/zshrc.d/50-prompt.zsh
@@ -1,21 +1,21 @@
-chpwd() { ls -hqp --color=auto }
-function command_not_found_handler(){
-	if [ -e /usr/bin/jp2a ];then
-		if [ -e ~/kirino.jpg ];then
-			jp2a ~/kirino.jpg -i
-		fi
-	fi
-	echo "ハァ…？$1とか何言ってんの？
-コマンドもろくに覚えられないなんて、アンタどうしようもないクズね。"
-}
+chpwd(){ ls -qp --color=auto }
+#function command_not_found_handler(){
+#	if [ -e /usr/bin/jp2a ];then
+#		if [ -e ~/kirino.jpg ];then
+#			jp2a ~/kirino.jpg -i
+#		fi
+#	fi
+#	echo "ハァ…？$1とか何言ってんの？
+#コマンドもろくに覚えられないなんて、アンタどうしようもないクズね。"
+#}
 
-if [ ${UID} -eq 0 ]; then
+#if [ ${UID} -eq 0 ]; then
 	#PROMPT="%K{green}%B%F{black}ROOT%b%k %F{cyan}%~ %f$ "
-else
-	UUU="%B%F{white}（」・ω・）」うー▶%b"
-	NYA="%B%F{white}\（・ω・\）にゃー！%b"
-	SAN="%B%F{red}＼（・ω・＼）SAN値！%b"
-	PIN="%B%F{red}（／・ω・）／ピンチ！%b"
+#else
+#	UUU="%B%F{white}（」・ω・）」うー▶%b"
+#	NYA="%B%F{white}\（・ω・\）にゃー！%b"
+#	SAN="%B%F{red}＼（・ω・＼）SAN値！%b"
+#	PIN="%B%F{red}（／・ω・）／ピンチ！%b"
 	#PROMPT="$PROMPT%K{green}%(?,${UUU},${SAN})%k%F{cyan}%~ %f$ ❯ "
 	#RPROMPT="$RPROMPT%K{green}%(?,${NYA},${PIN})%k${vcs_info_msg_0_}"
-fi
+#fi
