7874e04628cc5f5ec31d3e5feedc49d900b9afc2
onokatio <onokatio@gmail.com>
2018-02-23 10:44:53 +0900

    add hook system

---
+++ zsh/zshrc.d/hooks/anyenv.zsh
@@ -0,0 +1,37 @@
+	#eval "$(anyenv init - --no-rehash)"
+	#eval "$(anyenv init -)"
+	#anyenv() {
+	#	typeset command
+	#	command="$1"
+	#	if [ "$#" -gt 0 ]; then
+	#		shift
+	#	fi
+	#	command anyenv "$command" "$@"
+	#}
+
+if [[ -d $ANYENV_ROOT ]] ; then
+	echo "export PATH=$ANYENV_ROOT/bin:\$PATH"
+	xenvlist=(`command ls $ANYENV_ROOT/envs`)
+	for xenv in ${xenvlist[@]};do 
+		cat <<- EOS
+			export ${xenv}_ROOT='$ANYENV_ROOT/envs/${xenv}'
+			export PATH="$ANYENV_ROOT/${xenv}/bin:\$PATH"
+			export PATH="$ANYENV_ROOT/${xenv}/bin/shims:\$PATH"
+			hash -r 2>/dev/null || true
+			${xenv}() {
+				local command
+				command="\$1"
+				if [ "\$#" -gt 0 ]; then
+					shift
+				fi
+
+				case "\$command" in
+				rehash|shell)
+					hash -r 2>/dev/null || true;;
+				*)
+					command ${xenv} "\$command" "\$@";;
+					esac
+			}
+		EOS
+	done
+fi
