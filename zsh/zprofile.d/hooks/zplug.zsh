ab7e74379d51a6a3c6b78d8062323fbc25c9548c
onokatio <onokatio@gmail.com>
2018-05-09 14:36:40 +0900

    update

--- zsh/zprofile.d/hooks/zplug.zsh
+++ zsh/zprofile.d/hooks/zplug.zsh
@@ -1,17 +1,25 @@
-set -e  # stop when fail
-set -u  # fail when var is not defined
-
-DIST="../dists"
 
 cd `dirname $0`
 
-# export ZPLUG_HOME="../../zplug"
-export ZPLUG_REPOS=$DIST/.zsh-plugins
+export ZPLUG_HOME="$PWD/zplug"
+export ZPLUG_REPOS=$MYLOCAL_DIST/.zsh-plugins
 export ZPLUG_BIN=$ZPLUG_REPOS/bin
 
-#source $ZPLUG_HOME/init.zsh
-source ../../zplug/init.zsh
+source $ZPLUG_HOME/init.zsh
 
-while read line;do
-	eval "zplug $line"
-done < ./plugins.txt
+zplug "zsh-users/zsh-autosuggestions"
+zplug "zsh-users/zsh-completions", lazy:true
+
+zplug clear
+
+zplug check --verbose || zplug install
+zplug status
+
+zplug load --verbose
+
+list=(`zplug list`)
+
+for i in $list ; do
+	echo $i
+	zplug info $i
+done
