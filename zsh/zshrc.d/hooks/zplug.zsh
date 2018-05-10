e1cc6d8d54675150f7767e01f70be28a45bfd309
katio <onokatio@gmail.com>
2018-05-10 14:19:37 +0900

    zplugmove

---
+++ zsh/zshrc.d/hooks/zplug.zsh
@@ -0,0 +1,29 @@
+
+cd `dirname $0`
+
+export ZPLUG_HOME="$PWD/zplug"
+export ZPLUG_REPOS=$MYLOCAL_DIST/.zsh-plugins
+export ZPLUG_BIN=$ZPLUG_REPOS/bin
+
+source $ZPLUG_HOME/init.zsh
+
+zplug "zsh-users/zsh-autosuggestions"
+zplug "zsh-users/zsh-completions", lazy:true
+
+zplug clear
+
+zplug check --verbose >&2 || zplug install >&2
+zplug status >&2
+
+zplug load --verbose >&2
+
+list=(`zplug list`)
+
+echo "##### zplug #####"
+
+for i in $list ; do
+	eval $(zplug info $i | sed -e 's/-//' | sed -e 's/: /=/'|grep "dir" | sed 's/\x1b\[[0-9;]*m//g')
+	echo source $(ls $dir/*.zsh | sed -e 's/\.plugin\.zsh/\.zsh/' | sort | uniq)
+done
+
+echo "##### /zplug #####"
