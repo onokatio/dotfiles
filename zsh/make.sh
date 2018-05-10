32873e6a63f2910cae0444e107a0d4ce5e3a93d5
katio <onokatio@gmail.com>
2018-05-10 14:31:52 +0900

    update

--- zsh/make.sh
+++ zsh/make.sh
@@ -3,15 +3,16 @@
 set -e  # stop when fail
 set -u  # fail when var is not defined
 
-export MYLOCAL_DIST="$PWD/../dists"
 
 cd `dirname $0`
+
+export MYLOCAL_DIST="$PWD/../dists"
 
 function mes_info(){
 	tput setaf 11
 	echo -n $1
 	tput setaf 10
-	echo -n " $2..."
+	echo " $2..."
 	tput sgr0
 }
 function mes_done(){
@@ -24,11 +25,9 @@ mes_info '[zshrc]' 'init'
 	touch $MYLOCAL_DIST/zshrc
 	echo > $MYLOCAL_DIST/zshrc
 	rm -f ~/.zcompdump  # Clear zcomp cache
-mes_done
 
 mes_info '[zshrc]' 'Bundle zshrc.d'
 	command ls ./zshrc.d/*.zsh       | sort | xargs cat >> $MYLOCAL_DIST/zshrc
-mes_done
 
 mes_info '[zshrc]' 'Bundle hooks'
 	if which zsh >/dev/null;then
@@ -36,22 +35,21 @@ mes_info '[zshrc]' 'Bundle hooks'
 	else
 		echo "zsh not found"
 	fi
-mes_done
 
 mes_info '[zshrc]' 'Test & Compile'
 	zsh -n $MYLOCAL_DIST/zshrc
-	[[ -e ~/zshrc ]] && zsh -c "zcompile $MYLOCAL_DIST/zshrc"
-mes_done
+	zsh -c "zcompile $MYLOCAL_DIST/zshrc"
+
+mes_info '[zshrc]' 'Running zcomp...'
+  zsh -c "autoload -Uz compinit && compinit"
 
 
 mes_info '[zprofile]' 'init'
 	touch $MYLOCAL_DIST/zprofile
 	echo -n > $MYLOCAL_DIST/zprofile
-mes_done
 
 mes_info '[zprofile]' 'Bundle zprofile.d'
 	find ./zprofile.d -maxdepth 1 -name '*.zsh' | sort | xargs cat >> $MYLOCAL_DIST/zprofile
-mes_done
 
 mes_info '[zprofile]' 'Bundle hooks'
 	if which zsh >/dev/null;then
@@ -59,9 +57,7 @@ mes_info '[zprofile]' 'Bundle hooks'
 	else
 		echo "zsh not found"
 	fi
-mes_done
 
 mes_info '[zprofile]' 'Test & Compile'
 	zsh -n $MYLOCAL_DIST/zprofile
 	zsh -c "zcompile $MYLOCAL_DIST/zprofile"
-mes_done
