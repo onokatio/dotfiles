969d7a94ad481fa6e5bc97dc34557681486a22f1
onokatio <onokatio@gmail.com>
2018-06-08 13:19:58 +0900

    tmux-start

--- zsh/zshrc.d/00-exec.zsh
+++ zsh/zshrc.d/00-exec.zsh
@@ -1,14 +1,14 @@
 #zmodload zsh/zprof && zprof
 
 if [[ "$TTY" == "/dev/tty1" ]];then
-#	startx
+	startx > /dev/null 2>&1
 #	fbterm
 elif echo "$TTY" | grep "/dev/tty" ;then
 #	fbterm
 fi
 
 if which tmux >/dev/null 2>&1 && [[ -z "$TMUX" ]] && [[ -z ${REMOTEHOST}${SSH_CONNECTION} ]];then
-#	tmux
+	tmux
 fi
 
 umask 0022
