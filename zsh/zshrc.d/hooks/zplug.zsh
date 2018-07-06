49846af3e351e0701862106a0940f796c6b3a9de
onokatio <onokatio@gmail.com>
2018-07-06 22:22:01 +0900

    zplug-make

--- zsh/zshrc.d/hooks/zplug.zsh
+++ zsh/zshrc.d/hooks/zplug.zsh
@@ -14,11 +14,10 @@ source $ZPLUG_HOME/init.zsh
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-completions", lazy:true
 
-
-zplug check --verbose >&2 || zplug install >&2
+#zplug check --verbose >&2 || zplug install >&2
 zplug update >&2
 
-zplug load --verbose >&2
+#zplug load --verbose >&2
 zplug clear >&2
 
 list=(`zplug list`)
@@ -27,6 +26,7 @@ echo "##### zplug #####"
 
 for i in $list ; do
 	eval $(zplug info $i | sed -e 's/-//' | sed -e 's/: /=/'|grep "dir" | sed 's/\x1b\[[0-9;]*m//g')
+	echo find zsh file:  $(ls $dir/*.zsh | head -n1) >&2
 	echo source $(ls $dir/*.zsh | head -n1)
 done
 
