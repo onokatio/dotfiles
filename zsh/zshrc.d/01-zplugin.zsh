c15bf8bb2101f1b179c5cd264e82e1d3ed8e5d6e
onokatio <onokatio@gmail.com>
2018-10-12 10:21:38 +0900

    change zplugin

--- zsh/zshrc.d/01-zplugin.zsh
+++ zsh/zshrc.d/01-zplugin.zsh
@@ -3,11 +3,21 @@ ZPLGM[HOME_DIR]=$CTG/run/zplugin
 
 source $CTG/modules/zplugin/zplugin.zsh
 
-zplugin light zsh-users/zsh-autosuggestions
-zplugin light zdharma/fast-syntax-highlighting
+autoload -Uz _zplugin
+
+zplugin ice pick''
+zplugin light 'jsforce/jsforce-zsh-completions'
+
+zplugin ice pick''
 zplugin light zsh-users/zsh-completions
 
 autoload -Uz compinit
 compinit
 
 zplugin cdreplay -q # -q is for quiet
+
+zplugin ice wait'1' atload'_zsh_highlight'
+zplugin light zdharma/fast-syntax-highlighting
+
+zplugin ice wait'1' atload'_zsh_autosuggest_start'
+zplugin light zsh-users/zsh-autosuggestions
