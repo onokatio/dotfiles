ba9de707e3b95bb2206587ee58f2c9671b9fa9e1
onokatio <onokatio@gmail.com>
2018-10-12 13:12:38 +0900

    omz plugin

--- zsh/zshrc.d/11-zplugin.zsh
+++ zsh/zshrc.d/11-zplugin.zsh
@@ -5,11 +5,18 @@ source $CTG/modules/zplugin/zplugin.zsh
 
 autoload -Uz _zplugin
 
+zplugin snippet 'OMZ::lib/clipboard.zsh'
+zplugin snippet 'OMZ::lib/completion.zsh'
+zplugin snippet 'OMZ::lib/compfix.zsh'
+
+zplugin snippet 'OMZ::plugins/dotenv/dotenv.plugin.zsh'
+zplugin snippet 'OMZ::plugins/git/git.plugin.zsh'
+
 zplugin ice pick''
 zplugin light 'jsforce/jsforce-zsh-completions'
 
 zplugin ice pick''
-zplugin light zsh-users/zsh-completions
+zplugin light 'zsh-users/zsh-completions'
 
 autoload -Uz compinit
 compinit
