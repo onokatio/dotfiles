492961a1d0c0ec097dcfb98f9acd3daa81ee8e21
onokatio <onokatio@gmail.com>
2019-04-15 11:13:54 +0900

    aria2c

--- zsh/zshrc.d/40-alias.zsh
+++ zsh/zshrc.d/40-alias.zsh
@@ -40,3 +40,5 @@ alias reboot='echo dontuse'
 
 alias alpine="docker run -it --rm alpine:edge /bin/sh"
 alias ubuntu="docker run -it --rm ubuntu /bin/bash"
+
+alias aria2c="aria2c --bt-max-peers=0 --bt-request-peer-speed-limit=100M --max-connection-per-server=10 --split=10"
