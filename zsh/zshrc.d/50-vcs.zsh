224702c743b2f9a67cb92ef9d47ca75554a67114
onokatio <onokatio@gmail.com>
2019-05-21 09:35:02 +0900

    rename & split

--- zsh/zshrc.d/50-vcs.zsh
+++ zsh/zshrc.d/50-vcs.zsh
@@ -8,8 +8,6 @@
 #コマンドもろくに覚えられないなんて、アンタどうしようもないクズね。"
 #}
 
-chpwd(){ ls -qp --color=auto }
-
 preexec() {
   _pre="$1"
 }
@@ -28,30 +26,6 @@ precmd(){
 
 	return ${_r}
 }
-
-if [ ${UID} -eq 0 ]; then
-	PROMPT="%K{green}%B%F{black}ROOT%b%k %F{cyan}%~ %f$ "
-else
-	setopt prompt_subst
-	zstyle ':vcs_info:*' enable git
-	zstyle ':vcs_info:git:*' check-for-changes true
-	zstyle ':vcs_info:git:*' stagedstr "%K{white}%F{yellow}" # %c
-	zstyle ':vcs_info:git:*' unstagedstr "%K{white}%F{red}"  # %u
-	zstyle ':vcs_info:*' formats "%F{green}%K{white}%c%u[%b]%f"
-	zstyle ':vcs_info:*' actionformats '[%b|%a]'
-	#LANG=en_US.UTF-8 vcs_info
-	vcs_info
-	
-	local PNAME
-	if [[ -n "${REMOTEHOST}${SSH_CONNECTION}" ]];then
-		PNAME="%K{yellow}%F{black}SSH "
-	fi
-	UUU="%K{white}%F{black}（」・ω・）」うー"
-	NYA="＼（・ω・＼）にゃー！"
-	#SAN="＼（・ω・＼）SAN値！"
-	#PIN="（／・ω・）／ピンチ！"
-	PROMPT='${PNAME}%K{blue}%F{black} %~ %F{blue}%K{white}%k%F{blue}${vcs_info_msg_0_}%k%F{white}%f%k '
-fi
 
 do_enter() {
     if [[ -n $BUFFER ]]; then
