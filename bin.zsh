7ce01a142a1ec6798a930a7a91f99a42dbba9239
onokatio <onokatio@gmail.com>
2019-08-05 18:30:44 +0900

    file to bin.zsh

--- bin.zsh
+++ bin.zsh
@@ -5,22 +5,22 @@ zplugin ice as"program" make"PREFIX=$ZPFX" pick"$ZPFX/bin/git-issue"
 zplugin light dspinellis/git-issue
 
 zplugin ice as"program" pick"diff-so-fancy"
-zplugin snippet https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy
-
-#zplugin ice as'program' mv'yarn-1.10.1.js -> yarn' pick'yarn'
-#zplugin snippet https://github.com/yarnpkg/yarn/releases/download/v1.10.1/yarn-1.10.1.js
-
-#zplugin ice as'program' mv'nvim.appimage -> nvim' pick'nvim'
-#zplugin snippet 'https://github.com/neovim/neovim/releases/download/v0.3.1/nvim.appimage'
-
-zplugin ice as'program' atclone"mkdir -p ~/.themes && ./install.sh --dest ~/.themes --color light -s standard && rm -rf .git src"
-zplugin light nana-4/materia-theme
-
-zplugin ice as'program' atclone"mkdir -p ~/.icons && cat ./install.sh | DESTDIR=\"$HOME/.icons\" THEMES=Papirus sh"
-zplugin snippet 'https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/install.sh'
+zplugin snippet "https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy"
 
 zplugin ice wait'0' as'program' pick'bin/anyenv' atload'export ANYENV_ROOT=$PWD ; eval "$(anyenv init -)"'
 zplugin light riywo/anyenv
 
-zplugin ice as'program' pick'anyenv-update'
-zplugin snippet 'https://github.com/znz/anyenv-update/blob/master/bin/anyenv-update'
+zplugin ice as'program'
+zplugin snippet 'https://raw.githubusercontent.com/znz/anyenv-update/master/bin/anyenv-update'
+
+zplugin ice as'program'
+zplugin snippet 'https://raw.githubusercontent.com/upa/deadman/master/deadman'
+
+zplugin ice as'program'
+zplugin snippet 'https://raw.githubusercontent.com/fumiyas/home-commands/master/echo-sd'
+
+zplugin ice as'program'
+zplugin snippet 'https://raw.githubusercontent.com/simonwhitaker/gibo/master/gibo'
+
+zplugin ice as'program'
+zplugin snippet 'https://raw.githubusercontent.com/paulirish/git-open/master/git-open'
