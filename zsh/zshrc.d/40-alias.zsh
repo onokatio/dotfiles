64662311371ca6516d8a0a90c6a58a218c7e11c0
onokatio <onokatio@gmail.com>
2019-10-08 09:09:40 +0900

    alias wine

--- zsh/zshrc.d/40-alias.zsh
+++ zsh/zshrc.d/40-alias.zsh
@@ -45,3 +45,5 @@ alias ubuntu="docker run -it --rm ubuntu /bin/bash"
 alias aria2c="aria2c --bt-max-peers=0 --bt-request-peer-speed-limit=1000M --max-connection-per-server=16 --split=20 --bt-max-open-files=1000"
 
 alias CAPS='echo setxkbmap -option ctrl:swapcaps , setxkbmap -option'
+
+alias wine='LANG="ja_JP.UTF-8" ; wine'
