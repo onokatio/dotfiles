2c5647be51f38d8b71fbbe03dc5f1f74740323f0
onokatio <onokatio@gmail.com>
2020-01-01 03:31:27 +0900

    add config

---
+++ home-stow/.config/mozilla/user.js
@@ -0,0 +1,7 @@
+user_pref("app.normandy.enabled", false);
+user_pref("network.trr.bootstrapAddress", "8.8.8.8");
+user_pref("network.trr.custom_uri", "https://1.1.1.1/dns-query");
+user_pref("network.trr.mode", "https://1.1.1.1/dns-query");
+user_pref("network.trr.uri", "https://1.1.1.1/dns-query");
+user_pref("browser.safebrowsing.malware.enabled", false);
+user_pref("browser.safebrowsing.phishing.enabled", false);
