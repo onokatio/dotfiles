664822d2560b323a162a6c2aaebc1c6725967315
onokatio <onokatio@gmail.com>
2020-01-08 20:36:04 +0900

    fix firefox user.js (network.trr.mode)

--- home-stow/.config/mozilla/user.js
+++ home-stow/.config/mozilla/user.js
@@ -1,7 +1,8 @@
 user_pref("app.normandy.enabled", false);
+user_pref("network.security.esni.enabled", true);
 user_pref("network.trr.bootstrapAddress", "8.8.8.8");
 user_pref("network.trr.custom_uri", "https://1.1.1.1/dns-query");
-user_pref("network.trr.mode", "https://1.1.1.1/dns-query");
+user_pref("network.trr.mode", 2);
 user_pref("network.trr.uri", "https://1.1.1.1/dns-query");
 user_pref("browser.safebrowsing.malware.enabled", false);
 user_pref("browser.safebrowsing.phishing.enabled", false);
