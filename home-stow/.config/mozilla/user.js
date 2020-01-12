f6efd9c01db69a875f7b1eec3b5fa12175604525
onokatio <onokatio@gmail.com>
2020-01-12 16:26:50 +0900

    edit user.js

--- home-stow/.config/mozilla/user.js
+++ home-stow/.config/mozilla/user.js
@@ -20,4 +20,5 @@ user_pref("gfx.content.azure.backends", "direct2d1.1,skia");
 user_pref("gfx.direct2d.force-enabled", true);
 user_pref("gfx.text.subpixel-position.force-enabled", true);
 user_pref("gfx.webrender.compositor", true);
+user_pref("toolkit.legacyUserProfileCustomizations", true);
 user_pref("a", true);
