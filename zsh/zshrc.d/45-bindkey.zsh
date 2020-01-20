def0c8a9200c7c6dc96a0090b870727e8418414e
onokatio <onokatio@gmail.com>
2020-01-20 22:08:14 +0900

    remove unused & buggy feature from zshrc

--- zsh/zshrc.d/45-bindkey.zsh
+++ zsh/zshrc.d/45-bindkey.zsh
@@ -6,24 +6,6 @@ bindkey -e
 #}
 #add-zsh-hook preexec left_down_prompt_preexec
 
-function zle-keymap-select zle-line-init zle-line-finish
-{
-    case $KEYMAP in
-        main|viins)
-            PROMPT_2="$fg[cyan]-- INSERT --$reset_color"
-            ;;
-        vicmd)
-            PROMPT_2="$fg[white]-- NORMAL --$reset_color"
-            ;;
-        vivis|vivli)
-            PROMPT_2="$fg[yellow]-- VISUAL --$reset_color"
-            ;;
-    esac
-
-    BINDKEY_MODE="%{$terminfo_down_sc$PROMPT_2$terminfo[rc]%}"
-    zle reset-prompt
-}
-
 #zle -N zle-line-init
 #zle -N zle-line-finish
 #zle -N zle-keymap-select
