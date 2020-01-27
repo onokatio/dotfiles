d99ee7f4991533a53d892fbad8310aa511c3f1c1
onokatio <onokatio@gmail.com>
2020-01-27 10:03:35 +0900

    fix 1.1.1.1 and 8.8.8.8 rewrite issue

--- home-stow/.config/mozilla/user.js
+++ home-stow/.config/mozilla/user.js
@@ -1,6 +1,6 @@
 user_pref("app.normandy.enabled", false);
 user_pref("network.security.esni.enabled", true);
-user_pref("network.trr.bootstrapAddress", "8.8.8.8");
+user_pref("network.trr.bootstrapAddress", "1.1.1.1");
 user_pref("network.trr.custom_uri", "https://1.1.1.1/dns-query");
 user_pref("network.trr.mode", 2);
 user_pref("network.trr.uri", "https://1.1.1.1/dns-query");
