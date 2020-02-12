2bbd3981512be92c7a729d51705615ce15f3dbc6
onokatio <onokatio@gmail.com>
2020-02-12 18:51:51 +0900

    ssh key agent

--- zsh/zshrc.d/01-exec.zsh
+++ zsh/zshrc.d/01-exec.zsh
@@ -10,7 +10,7 @@ fi
 
 #umask 0022
 
-unset SSH_AGENT_PID
-if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
-fi
+#unset SSH_AGENT_PID
+#if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+#  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+#fi
