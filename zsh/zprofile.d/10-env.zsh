023bf73d36c0d4f3d65df353e0f8612d4925ea63
onokatio <onokatio@gmail.com>
2020-04-10 19:09:19 +0900

    :sparkles: add comment

--- zsh/zprofile.d/10-env.zsh
+++ zsh/zprofile.d/10-env.zsh
@@ -7,16 +7,21 @@
 
 export NO_AT_BRIDGE=1
 export EMOJI_CLI_KEYBIND=^f
+
+##### Homebrew #####
 export HOMEBREW_CACHE=/tmp
 export HOMEBREW_MAKE_JOBS=4
 export HOMEBREW_FORCE_BREWED_CURL=1
 export HOMEBREW_EDITOR=nvim
 #export HOMEBREW_CC="ccache gcc"
 #export HOMEBREW_CXX="ccache g++"
-
 #export HOMEBREW_NO_ENV_FILTERING=1
+#export HOMEBREW_CURL=$(which curl)
 
 #export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+
+
+##### Shell #####
 export EDITOR=nvim
 #ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor root line)
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main pattern cursor root line)
@@ -24,6 +29,7 @@ ZSH_HIGHLIGHT_HIGHLIGHTERS=(main pattern cursor root line)
 ZSH_AUTOSUGGEST_STRATEGY=match_prev_cmd
 ZSH_AUTOSUGGEST_USE_ASYNC=1
 ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
+
 export USE_CCACHE=1
 export CCACHE_COMPRESS=1
 export CCACHE_DIR=$HOME/.ccache
@@ -33,13 +39,6 @@ export MAKE_OPTS="-j 4"
 export PROOT_NO_SECCOMP=1
 export XZ_DEFAULTS="--threads 4"
 
-#export POWERLEVEL9K_PROMPT_ON_NEWLINE=true
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(custom_uuu dir root_indicator ssh)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(vcs custom_nya)
-POWERLEVEL9K_CUSTOM_UUU="echo （」・ω・）」うー"
-POWERLEVEL9K_CUSTOM_NYA="echo ＼（・ω・＼）にゃー！"
-
-#export HOMEBREW_CURL=$(which curl)
 
 export GOPATH=$HOME/gopath
 export PATH=$GOPATH/bin:$PATH
@@ -50,8 +49,6 @@ export PATH=$GOPATH/bin:$PATH
 
 export NVM_DIR="$CTG/modules/nvm"
 
-export MOZ_USE_XINPUT2=1
-
 export VGL_READBACK=pbo
 
 #export ZIPINFOOPT="-OCP932"
@@ -61,6 +58,7 @@ export WINEDEBUG=+wgl
 
 export MOZ_ACCELERATED=1
 export MOZ_WEBRENDER=1
+export MOZ_USE_XINPUT2=1
 
 export GO111MODULE=on
 
@@ -75,10 +73,17 @@ export N0CLI_API_URL=grpc://172.16.1.11:20180
 
 export QT_QPA_PLATFORMTHEME=qt5ct
 
+##### GUI #####
+
 #export GTK2_RC_FILES=/usr/share/themes/Materia-light/gtk-2.0/gtkrc
 #export GTK_THEME=Materia-light
 
-# disable wayland power until firefox can't manage it.
+# disable wayland power while we can't manage it.
+# with wayland,
+# - fcitx & mozc doesn't work,
+# - forefox freezes a lot times.
 #export MOZ_ENABLE_WAYLAND=1
-
 #export XDG_SESSION_TYPE=wayland
+
+# print firefox vaapi debug message.
+export MOZ_LOG="PlatformDecoderModule:5"
