708d5d72bfd01e1e9310c88ae54a0bffd1e7c921
onokatio <onokatio@gmail.com>
2020-04-25 01:45:20 +0900

    WLR device & language

--- zsh/zshrc.d/01-exec.zsh
+++ zsh/zshrc.d/01-exec.zsh
@@ -2,12 +2,14 @@
 
 if [[ "$TTY" == "/dev/tty1" ]];then
 	#startx -- -logverbose 9 -verbose 9 2>&1 | tee ~/log/startx.log
-	export XDG_SESSION_TYPE=wayland
+	#export XDG_SESSION_TYPE=wayland
+	#export WINIT_UNIX_BACKEND=wayland
 	#WLR_DRM_DEVICES=/dev/dri/by-path/pci-0000:00:02.0-card
 	#WLR_DRM_DEVICES=/dev/dri/by-path/pci-0000:01:00.0-card
-	#WLR_DRM_DEVICES=/dev/dri/card0:/dev/dri/card1
-	sway -d -V --my-next-gpu-wont-be-nvidia 2>&1 | tee ~/log/sway.log | systemd-cat -t 'katio.sway'
-	export XDG_SESSION_TYPE=
+	export LANG=ja_JP.UTF-8
+	WLR_DRM_DEVICES=/dev/dri/card0:/dev/dri/card1 sway -d -V --my-next-gpu-wont-be-nvidia 2>&1 | tee ~/log/sway.log | systemd-cat -t 'katio.sway'
+	export LANG=en_US.UTF-8
+	#export XDG_SESSION_TYPE=
 fi
 
 if [[ -z "$TMUX" ]] && [[ -z ${REMOTEHOST}${SSH_CONNECTION} ]];then
