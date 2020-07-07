084de552816397bc28abd69820738dcf146d2c9b
onokatio <onokatio@gmail.com>
2020-07-07 10:55:09 +0900

    alias for alternate tools

---
+++ zsh/zshrc.d/41-alternative-alias.zsh
@@ -0,0 +1,8 @@
+alias ls='exa --all --binary --group --header --links --inode --git --icons --classify --group-directories-first --git'
+alias cat='bat --theme="Solarized (dark)"'
+alias find='fd'
+#alias ps='procs'
+alias grep='rg'
+alias diff='diff-so-fancy-standalone'
+alias git='hub'
+alias ssh='xxh'
