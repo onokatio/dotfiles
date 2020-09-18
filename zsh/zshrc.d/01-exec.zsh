987e421575e2841753d81e3e3c684c5950cfdaa5
onokatio <onokatio@gmail.com>
2020-09-18 11:23:29 +0900

    update terminal title

--- zsh/zshrc.d/01-exec.zsh
+++ zsh/zshrc.d/01-exec.zsh
@@ -39,3 +39,14 @@ AGENT_TIMEOUT=3600
 
 typeset -U path PATH
 export CCACHE_PATH="/usr/bin"
+
+case ${TERM} in
+
+  xterm*|rxvt*|Eterm|aterm|kterm|gnome*)
+     PROMPT_COMMAND=${PROMPT_COMMAND:+$PROMPT_COMMAND; }'printf "\033]0;%s@%s:%s\007" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/\~}"'
+
+    ;;
+  screen*)
+    PROMPT_COMMAND=${PROMPT_COMMAND:+$PROMPT_COMMAND; }'printf "\033_%s@%s:%s\033\\" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/\~}"'
+    ;;
+esac
