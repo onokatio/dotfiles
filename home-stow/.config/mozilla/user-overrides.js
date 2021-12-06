6600cae01bfc57fbb3fdb4fd752ba81e517c1936
onokatio <onokatio@gmail.com>
2021-12-07 02:00:12 +0900

    firefox user.js

--- home-stow/.config/mozilla/user-overrides.js
+++ home-stow/.config/mozilla/user-overrides.js
@@ -1,4 +1,3 @@
-user_pref("app.normandy.enabled", false);
 //user_pref("network.security.esni.enabled", true);
 user_pref("network.trr.bootstrapAddress", "1.1.1.1");
 user_pref("network.trr.custom_uri", "https://security.cloudflare-dns.com/dns-query");
@@ -30,4 +29,17 @@ user_pref("layout.css.grid-template-masonry-value.enabled", true);
 user_pref("fission.autostart", true);
 //user_pref("media.ffvpx.enabled", false);
 // now firefox beta use software even wayland/vaapi is enabled.
-user_pref("a", true);
+
+//override arkenfox
+user_pref("browser.startup.page", 3);
+user_pref("privacy.clearOnShutdown.history", false);
+user_pref("keyword.enabled", true);
+user_pref("intl.accept_languages", "ja");
+user_pref("browser.search.suggest.enabled", true);
+user_pref("browser.urlbar.suggest.searches", true);
+user_pref("browser.cache.disk.enable", true);
+user_pref("media.eme.enabled", true);
+user_pref("webgl.disabled", false);
+user_pref("browser.translation.detectLanguage", true)
+user_pref("browser.translation.engine", "Google")
+user_pref("browser.translation.ui.show", true)
