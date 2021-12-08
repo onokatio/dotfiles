554d47790cc95b7efa44c1de6886f9cc9d996671
onokatio <onokatio@gmail.com>
2021-12-08 18:40:33 +0900

    firefox user.js

--- home-stow/.config/mozilla/user-overrides.js
+++ home-stow/.config/mozilla/user-overrides.js
@@ -26,7 +26,7 @@ user_pref("browser.warnOnQuit", true);
 //user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("image.avif.enabled", true);
 user_pref("layout.css.grid-template-masonry-value.enabled", true);
-user_pref("fission.autostart", true);
+//user_pref("fission.autostart", true); //avaliable on nightly only
 //user_pref("media.ffvpx.enabled", false);
 // now firefox beta use software even wayland/vaapi is enabled.
 
@@ -34,12 +34,11 @@ user_pref("fission.autostart", true);
 user_pref("browser.startup.page", 3);
 user_pref("privacy.clearOnShutdown.history", false);
 user_pref("keyword.enabled", true);
-user_pref("intl.accept_languages", "ja");
 user_pref("browser.search.suggest.enabled", true);
 user_pref("browser.urlbar.suggest.searches", true);
-user_pref("browser.cache.disk.enable", true);
+//user_pref("browser.cache.disk.enable", true);
 user_pref("media.eme.enabled", true);
-user_pref("webgl.disabled", false);
+//user_pref("webgl.disabled", false);
 user_pref("browser.translation.detectLanguage", true)
 user_pref("browser.translation.engine", "Google")
 user_pref("browser.translation.ui.show", true)
