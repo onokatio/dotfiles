9c7be36f5a8f78477d706058117a5410b2086915
onokatio <onokatio@gmail.com>
2022-12-21 15:10:01 +0900

    disble startup vcs

--- zsh/zshrc.d/55-prompt.zsh
+++ zsh/zshrc.d/55-prompt.zsh
@@ -31,7 +31,7 @@ else
 	zstyle ':vcs_info:*' formats "%F{green}%K{white}%c%u[%b]%f"
 	zstyle ':vcs_info:*' actionformats '[%b|%a]'
 	#LANG=en_US.UTF-8 vcs_info
-	vcs_info
+	#vcs_info
 
 	local PNAME
 	if [[ -n "${REMOTEHOST}${SSH_CONNECTION}" ]]; then
