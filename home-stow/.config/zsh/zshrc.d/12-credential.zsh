4ad49d6f43ec3d08e8dffc0db483bea80818c482
onokatio <onokatio@gmail.com>
2023-06-15 20:25:47 +0900

    cred

--- home-stow/.config/zsh/zshrc.d/12-credential.zsh
+++ home-stow/.config/zsh/zshrc.d/12-credential.zsh
@@ -1,12 +1,15 @@
 function inject_credential(){
 	s=${3[(w)1]} # get first element of $3
 	if [[ "$s" == "rclone" || "$s" == "rclone-mac" || "$s" == "env" ]];then
-		export RCLONE_CONFIG_WASABI_ACCESS_KEY_ID=$(bws get secret 9815a28f-b01d-4553-aaec-b00c0072cf14 | jq -r .value)
-		export RCLONE_CONFIG_WASABI_SECRET_ACCESS_KEY=$(bws get secret c6eac4b4-effc-467e-80a0-b00c007327df | jq -r .value)
-		export RCLONE_CONFIG_WASABI_US_ACCESS_KEY_ID=$(bws get secret 9815a28f-b01d-4553-aaec-b00c0072cf14 | jq -r .value)
-		export RCLONE_CONFIG_WASABI_US_SECRET_ACCESS_KEY=$(bws get secret c6eac4b4-effc-467e-80a0-b00c007327df | jq -r .value)
-		export RCLONE_CONFIG_WASABI_TOKYO_CRYPT_PASSWORD=$(bws get secret 0c5c07f6-dff0-416a-9ee1-b00c00954b24 | jq -r .value)
-		export RCLONE_CONFIG_WASABI_CRYPT_PASSWORD=$(bws get secret e5516708-650b-4ce6-b408-b02200b1d488 | jq -r .value)
+		echo "[zsh] detecting env injection: $s"
+		echo ""
+		BWS_OUTPUTS="$(bws list secrets)"
+		export RCLONE_CONFIG_WASABI_ACCESS_KEY_ID=$(echo $BWS_OUTPUTS | jq -r '.[] | select(.key=="wasabi-access-key") | .value')
+		export RCLONE_CONFIG_WASABI_SECRET_ACCESS_KEY=$(echo $BWS_OUTPUTS | jq -r '.[] | select(.key=="wasabi-access-token") | .value')
+		export RCLONE_CONFIG_WASABI_US_ACCESS_KEY_ID=$(echo $BWS_OUTPUTS | jq -r '.[] | select(.key=="wasabi-crypt-access-key") | .value')
+		export RCLONE_CONFIG_WASABI_US_SECRET_ACCESS_KEY=$(echo $BWS_OUTPUTS | jq -r '.[] | select(.key=="wasabi-access-token") | .value')
+		export RCLONE_CONFIG_WASABI_TOKYO_CRYPT_PASSWORD=$(echo $BWS_OUTPUTS | jq -r '.[] | select(.key=="wasabi-tokyo-crypt") | .value')
+		export RCLONE_CONFIG_WASABI_CRYPT_PASSWORD=$(echo $BWS_OUTPUTS | jq -r '.[] | select(.key=="wasabi-crypt") | .value')
 	fi
 }
 
