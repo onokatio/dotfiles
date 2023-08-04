8da82cc5c619414bc0adf6e3c5a62db9f876085c
onokatio <onokatio@gmail.com>
2023-08-04 18:42:25 +0900

    add aws

--- home-stow/.config/zsh/zshrc.d/12-credential.zsh
+++ home-stow/.config/zsh/zshrc.d/12-credential.zsh
@@ -1,6 +1,6 @@
 function inject_credential(){
 	s=${3[(w)1]} # get first element of $3
-	if [[ "$s" == "rclone" || "$s" == "rclone-mac" || "$s" == "env" ]];then
+	if [[ "$s" == "rclone" || "$s" == "rclone-mac" || "$s" == "env" || "$s" == "aws" || "$s" == "terraform" ]];then
 		echo "[zsh] detecting env injection: $s"
 		echo ""
 		BWS_OUTPUTS="$(bws list secrets)"
@@ -10,6 +10,8 @@ function inject_credential(){
 		export RCLONE_CONFIG_WASABI_US_SECRET_ACCESS_KEY=$(echo $BWS_OUTPUTS | jq -r '.[] | select(.key=="wasabi-access-token") | .value')
 		export RCLONE_CONFIG_WASABI_TOKYO_CRYPT_PASSWORD=$(echo $BWS_OUTPUTS | jq -r '.[] | select(.key=="wasabi-tokyo-crypt") | .value')
 		export RCLONE_CONFIG_WASABI_CRYPT_PASSWORD=$(echo $BWS_OUTPUTS | jq -r '.[] | select(.key=="wasabi-crypt") | .value')
+		export AWS_ACCESS_KEY_ID=$(echo $BWS_OUTPUTS | jq -r '.[] | select(.key=="planeta-aws-key") | .value')
+		export AWS_SECRET_ACCESS_KEY=$(echo $BWS_OUTPUTS | jq -r '.[] | select(.key=="planeta-aws-secret") | .value')
 	fi
 }
 
