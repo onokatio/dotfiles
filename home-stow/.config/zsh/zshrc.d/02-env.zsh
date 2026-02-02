8807d2d861464c1d6dff28b55570a2b8a04787d1
onokatio <contact@katio.net>
2026-02-02 19:44:29 +0900

    update

--- home-stow/.config/zsh/zshrc.d/02-env.zsh
+++ home-stow/.config/zsh/zshrc.d/02-env.zsh
@@ -9,27 +9,18 @@ export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME:+$FUNCNAME(): }'
 export HOMEBREW_API_AUTO_UPDATE_SECS=120
 export HOMEBREW_BOOTSNAP=1
 export HOMEBREW_DISPLAY_INSTALL_TIMES=1
-export HOMEBREW_FORCE_BREWED_CURL=1
-export HOMEBREW_FORCE_BREWED_GIT=1
-#export HOMEBREW_FORCE_VENDOR_RUBY=1
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_BREWFILE_ON_REQUEST=1
-#export HOMEBREW_BREWFILE_LEAVES=1
 
 ##### Shell #####
 export EDITOR=nvim
-export BROWSER=firefox-developer-edition
+#export BROWSER=firefox-developer-edition
 
 export USE_CCACHE=1
 export CCACHE_COMPRESS=1
-export CCACHE_DIR=$HOME/.ccache
-#which ccache >/dev/null 2>&1 && export CC="ccache gcc"
 export MAKE_OPTS="-j 4"
 export PROOT_NO_SECCOMP=1
 export XZ_DEFAULTS="--threads 4"
-
-#export GOPATH=$HOME/gopath
-#export PATH=$GOPATH/bin:$PATH
 
 #export npm_config_prefix=~/.node_modules
 
@@ -44,8 +35,8 @@ export WINEDEBUG=-all,+wgl
 
 export PIPENV_VENV_IN_PROJECT=true
 
-export VOLTA_HOME="$HOME/.volta"
-export PATH="$VOLTA_HOME/bin:$PATH"
+#export VOLTA_HOME="$HOME/.volta"
+#export PATH="$VOLTA_HOME/bin:$PATH"
 
 ##### GUI #####
 
@@ -87,7 +78,6 @@ export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 export XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
 export XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
 export XDG_STATE_HOME=${XDG_STATE_HOME:-$HOME/.local/state}
-#export XDG_PICTURES_DIR=$HOME/Downloads/Picture
 export WGETRC="$XDG_CONFIG_HOME/wgetrc"
 export MYSQL_HISTFILE="$XDG_DATA_HOME"/mysql_history
 export SQLITE_HISTORY=$XDG_DATA_HOME/sqlite_history
@@ -99,8 +89,6 @@ export GEM_HOME="$XDG_DATA_HOME"/gem
 export GEM_SPEC_CACHE="$XDG_CACHE_HOME"/gem
 export VAGRANT_HOME="$XDG_DATA_HOME"/vagrant
 export VAGRANT_ALIAS_FILE="$XDG_DATA_HOME"/vagrant/aliases
-#export TERMINFO="$XDG_DATA_HOME"/terminfo
-#export TERMINFO_DIRS="$XDG_DATA_HOME"/terminfo:/usr/share/terminfo
 export WINEPREFIX="$XDG_DATA_HOME"/wineprefixes/default
 #export KUBECONFIG="$XDG_DATA_HOME"/kube/config
 export TF_CLI_CONFIG_FILE="$XDG_CONFIG_HOME"/terraformrc
@@ -119,7 +107,6 @@ export PYTHONHISTFILE="/dev/null"
 export PYTHON_HISTORY="/dev/null"
 alias nvidia-settings='nvidia-settings --config="$XDG_CONFIG_HOME"/nvidia/settings'
 #alias yarn='yarn --use-yarnrc "$XDG_CONFIG_HOME/yarn/config"'
-export ANYENV_ROOT="$XDG_DATA_HOME/anyenv"
 export BUNDLE_USER_CONFIG="$XDG_CONFIG_HOME"/bundle
 export BUNDLE_USER_CACHE="$XDG_CACHE_HOME"/bundle
 export BUNDLE_USER_PLUGIN="$XDG_DATA_HOME"/bundle
