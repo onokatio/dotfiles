eb4cd87dcf234f39690b1af02992ae21c7113e75
onokatio <contact@katio.net>
2026-03-20 17:48:41 +0900

    remove

--- home-stow/.config/zsh/zshrc.d/05-exec.zsh
+++ home-stow/.config/zsh/zshrc.d/05-exec.zsh
@@ -3,7 +3,7 @@
 #export RUSTC_WRAPPER="${RUSTC_WRAPPER:-`which sccache`}"
 #export CARGO_HOME="$XDG_DATA_HOME"/cargo
 
-export HISTFILE="$XDG_STATE_HOME"/history
+export HISTFILE="${XDG_STATE_HOME:-$HOME/.local/state}"/history
 export HISTSIZE=1000000
 export SAVEHIST=1000000
 #REPORTTIME=3
@@ -13,27 +13,17 @@ if [[ "$TTY" == "/dev/tty1" ]]; then
 	#startx -- -logverbose 9 -verbose 9 2>&1 | tee ~/log/startx.log
 	#export XDG_SESSION_TYPE=wayland
 	#export WINIT_UNIX_BACKEND=wayland
-	#WLR_DRM_DEVICES=/dev/dri/by-path/pci-0000:00:02.0-card
-	#WLR_DRM_DEVICES=/dev/dri/by-path/pci-0000:01:00.0-card
 	export LANG=ja_JP.UTF-8
 	export XDG_CURRENT_DESKTOP=sway
 	export XDG_SESSION_TYPE=wayland
-	#WLR_DRM_DEVICES=/dev/dri/card0:/dev/dri/card1 sway -d -V --my-next-gpu-wont-be-nvidia 2>&1 > ~/sway.log
-	#WLR_DRM_DEVICES=/dev/dri/card0 sway -d -V --my-next-gpu-wont-be-nvidia 2>&1 | tee ~/sway.log
-	rm -f ~/sway.log
-	#sway -d -V --my-next-gpu-wont-be-nvidia 2>&1 | tee ~/sway.log
-	#export WLC_BG=0
 	#export WLR_NO_HARDWARE_CURSORS=1
 	sway --my-next-gpu-wont-be-nvidia >~/sway.log 2>&1
 	#| systemd-cat -t 'katio.sway' > /dev/null
 	#| systemd-cat -t 'katio.sway'
-	export LANG=en_US.UTF-8
-	export XDG_CURRENT_DESKTOP=
-	export XDG_SESSION_TYPE=tty
-	#export XDG_SESSION_TYPE=
+	#export LANG=en_US.UTF-8
+	#export XDG_CURRENT_DESKTOP=
+	#export XDG_SESSION_TYPE=tty
 fi
-
-#umask 0022
 
 #unset SSH_AGENT_PID
 #if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
@@ -48,17 +38,6 @@ fi
 
 typeset -U path PATH
 export CCACHE_PATH="/usr/bin"
-
-case ${TERM} in
-
-xterm* | rxvt* | Eterm | aterm | kterm | gnome*)
-	PROMPT_COMMAND=${PROMPT_COMMAND:+$PROMPT_COMMAND; }'printf "\033]0;%s@%s:%s\007" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/\~}"'
-
-	;;
-screen*)
-	PROMPT_COMMAND=${PROMPT_COMMAND:+$PROMPT_COMMAND; }'printf "\033_%s@%s:%s\033\\" "${USER}" "${HOSTNAME%%.*}" "${PWD/#$HOME/\~}"'
-	;;
-esac
 
 if [[ ! -z "$command[brew]" ]];then
 	HB_CNF_HANDLER="$(brew --repository)/Library/Taps/homebrew/homebrew-command-not-found/handler.sh"
